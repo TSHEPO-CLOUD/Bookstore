b00465d730e292f94f6fac62a32f11be93601037
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-02 12:34:58 +0200

    Add button

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,6 +8,27 @@ function Page() {
   const { books } = booksReducer;
   const [booksDisplay, setBooksDisplay] = useState(null);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (books !== booksDisplay) {
+      setBooksDisplay(books);
+    }
+  });
+
+  if (booksDisplay) {
+    return (
+      <div className="books-container">
+        {booksDisplay.map((element) => (
+          <div className="bookcard" key={element.title}>
+            <div className="book-left">
+              <h4 className="book_genre">{element.category}</h4>
+              <h3 className="title">{element.title}</h3>
+              <h6 className="author">{element.author}</h6>
+              <div className="actions">
+                <button type="button" className="btn-special">
+                  Comments
+                </button>
+
 const books = [
   {
     category: 'Commedy',
